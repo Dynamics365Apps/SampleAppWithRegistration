ce1f01b6fec29f4a8df80bb43cc58da4d4172959
waldo <github@waldo.be>
2017-02-15 17:18:31 +0100

    Initial Commit

---
+++ PowerShell Scripts/3_CreatePermissionSets.ps1
@@ -0,0 +1,15 @@
+﻿$PermissionSets = @()
+
+New-NAVAppPermissionSet `
+    -ServerInstance $ModifiedServerInstance `    -AppName $AppName `    -PermissionType Read `    -OnTableIDs 50000
+
+New-NAVAppPermissionSet `
+    -ServerInstance $ModifiedServerInstance `    -AppName $AppName `    -PermissionType Write `    -OnTableIDs 50000
+
+<#
+List of table you should probably handle!
+
+$Tables = Invoke-NAVSQL -ServerInstance $ModifiedServerInstance -SQLCommand 'select * from Object where Modified=1 and Type=1'
+$Tables.ID
+
+#>
